5f991897d634e24a5a37ded0610806c874a86017
Sergei Tsarik <49017393+github4ta@users.noreply.github.com>
2023-05-25 13:39:46 +0300

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -11,3 +11,17 @@
       });
    }
 })(document);
+
+(function(document) {
+   var shareButtons = document.querySelectorAll(".fa[data-network]");
+   for(var i = 0; i < shareButtons.length; i++) {
+      var shareButton = shareButtons[i];
+      
+      shareButton.addEventListener("click", function(e) {
+         var elm = e.target;
+         var network = elm.dataset.network;
+         
+         console.log("share click: " + network);
+      });
+   }
+})(document);
