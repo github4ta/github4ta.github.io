9c0b09ac627bfd77d7061681c769c45b49c7a7e8
Sergei Tsarik <49017393+github4ta@users.noreply.github.com>
2023-05-25 13:49:51 +0300

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,19 +1,4 @@
 (function(document) {
-   var shareButtons = document.querySelectorAll(".st-custom-button[data-network]");
-   for(var i = 0; i < shareButtons.length; i++) {
-      var shareButton = shareButtons[i];
-      
-      shareButton.addEventListener("click", function(e) {
-         var elm = e.target;
-         var network = elm.dataset.network;
-         
-         console.log("share click: " + network);
-      });
-   }
-})(document);
-
-
-(function(document) {
    var shareButtons = document.querySelectorAll(".fa[data-network]");
    for(var i = 0; i < shareButtons.length; i++) {
       var shareButton = shareButtons[i];
